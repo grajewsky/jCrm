d75386ca34bceb184fbc2594e277a326a6c36841
Grajewsky <grajewski.lukasz@wp.pl>
2017-10-29 23:58:43 +0100

    Initial project

---
+++ src/main/java/org/jcrm/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package org.jcrm.config.audit;
---
+++ src/main/webapp/app/services/auth/password.service.js
@@ -0,0 +1,15 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('jCrmApp')
+        .factory('Password', Password);
+
+    Password.$inject = ['$resource'];
+
+    function Password($resource) {
+        var service = $resource('api/account/change-password', {}, {});
+
+        return service;
+    }
+})();
